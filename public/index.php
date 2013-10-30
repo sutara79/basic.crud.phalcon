69e75b8cc3ad67e1cc06a0255524035c1fe0fc02
Yuusaku Miyazaki <toumin.m7@gmail.com>
2013-10-31 08:45:03 +0900

    v0.1

--- public/index.php
+++ public/index.php
@@ -1,121 +1,45 @@
 <?php
-
-try {
-
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-
-} catch(\Phalcon\Exception $e) {
-     echo "1.: ", $e->getMessage();
-}
-
-try {
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
-
-} catch(\Phalcon\Exception $e) {
-     echo "2.: ", $e->getMessage();
-}
-
-try {
-    //Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
-
-} catch(\Phalcon\Exception $e) {
-     echo "3.: ", $e->getMessage();
-}
-
-try {
-    // DB設定
-    $di->set('db', function() {
-    	return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(
-				'dbname' => 'db/test.sqlite3'
-			));
-		});
-
-} catch(\Phalcon\Exception $e) {
-     echo "4.: ", $e->getMessage();
-}
-
-try {
-		// BaseURI設定
-		$di->set('url', function(){
-		    $url = new Phalcon\Mvc\Url();
-		    $url->setBaseUri('/Phalcon/');
-		    return $url;
-		});
-
-} catch(\Phalcon\Exception $e) {
-     echo "5.: ", $e->getMessage();
-}
-
-try {
-    //Setting up the view component
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
-
-} catch(\Phalcon\Exception $e) {
-     echo "6.: ", $e->getMessage();
-}
-
-try {
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
-
-} catch(\Phalcon\Exception $e) {
-     echo "7.: ", $e->getMessage();
-}
-
-try {
-    echo $application->handle()->getContent();
-
-} catch(\Phalcon\Exception $e) {
-     echo "8.: ", $e->getMessage();
-}
+phpinfo();
 /*
 try {
 
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
+	//Register an autoloader
+	$loader = new \Phalcon\Loader();
 
-    //Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
+	$loader->registerDirs(array(
+		'../app/controllers/',
+		'../app/models/'
+	))->register();
+ 
+	//Create a DI
+	$di = new Phalcon\DI\FactoryDefault();
 
-    // DB設定
-    $di->set('db', function() {
-    	return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(
-				'dbname' => 'db/test.sqlite3'
-			));
-		});
+	// DB設定
+	$di->set('db', function() {
+		return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(
+			'dbname' => 'db/test.sqlite3'
+		));
+	});
 
-		// BaseURI設定
-		$di->set('url', function(){
-		    $url = new Phalcon\Mvc\Url();
-		    $url->setBaseUri('/Phalcon/');
-		    return $url;
-		});
+	// BaseURI設定
+	$di->set('url', function(){
+		$url = new Phalcon\Mvc\Url();
+		$url->setBaseUri('/Phalcon/');
+		return $url;
+	});
 
-    //Setting up the view component
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
+	//Setting up the view component
+	$di->set('view', function(){
+		$view = new \Phalcon\Mvc\View();
+		$view->setViewsDir('../app/views/');
+		return $view;
+	});
 
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
+	//Handle the request
+	$application = new \Phalcon\Mvc\Application($di);
 
-    echo $application->handle()->getContent();
+	echo $application->handle()->getContent();
 
 } catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
+	 echo "PhalconException: ", $e->getMessage();
 }*/
