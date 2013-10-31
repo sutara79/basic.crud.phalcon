88e6522f075be3488d4dd6abc676db89e0060dcd
Yuusaku Miyazaki <toumin.m7@gmail.com>
2013-11-01 08:28:42 +0900

    v0.1

---
+++ app/views/entries/edit.php
@@ -0,0 +1,11 @@
+<?php print form_open("entries/edit/{$row->id}", array('role' => 'form')) ?>
+	<div class="form-group">
+		<?php print form_label(lang('field_name'), 'name') ?>
+		<?php print form_input('name', htmlspecialchars($row->name), 'class="form-control"') ?>
+	</div>
+	<div class="form-group">
+		<?php print form_label(lang('field_body'), 'body') ?>
+		<?php print form_textarea('body', htmlspecialchars($row->body), 'class="form-control"') ?>
+	</div>
+	<?php print form_submit('submit', lang('view_submit'), 'class="btn btn-primary"') ?>
+<?php print form_close() ?>
