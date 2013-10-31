ffdb725c264ebff33e7b792a30330b51b8d9d034
Yuusaku Miyazaki <toumin.m7@gmail.com>
2013-10-31 12:23:47 +0900

    v0.1

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
-echo __DIR__;
+$extensions = get_loaded_extensions();
+$necessary = array('Core', 'libxml', 'filter', 'SPL', 'standard', 'phalcon', 'pdo_mysql');
+$flag = true;
+for ($i=0; $i<count($necessary); $i++) {
+	if (!in_array($necessary[$i], $extensions)) {
+		$flag = false;
+		print "<strong>{$necessary[$i]}</strong> did not loaded.<br>";
+	}
+}
+if ($flag) print '<strong>All of necessaries has loaded.</strong>';
 error_reporting(E_ALL);
 
 try {
