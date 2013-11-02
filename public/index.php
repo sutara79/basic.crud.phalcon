3fba156dc6a5b15d4880bca2c445513d8083050c
Yuusaku Miyazaki <toumin.m7@gmail.com>
2013-11-02 17:22:45 +0900

    v0.4

--- public/index.php
+++ public/index.php
@@ -2,42 +2,50 @@
 
 try {
 
-    //Register an autoloader
-    $loader = new \Phalcon\Loader();
-    $loader->registerDirs(array(
-        '../app/controllers/',
-        '../app/models/'
-    ))->register();
+	//Register an autoloader
+	$loader = new \Phalcon\Loader();
+	$loader->registerDirs(array(
+		'../app/controllers/',
+		'../app/models/'
+	))->register();
 
-    //Create a DI
-    $di = new Phalcon\DI\FactoryDefault();
+	//Create a DI
+	$di = new Phalcon\DI\FactoryDefault();
 
-    // DB設定
-    $di->set('db', function() {
-    	return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(
-				'dbname' => 'db/test.sqlite3'
-			));
-		});
-/*
-		// BaseURI設定
-		$di->set('url', function(){
-		    $url = new Phalcon\Mvc\Url();
-		    $url->setBaseUri('/Phalcon/');
-		    return $url;
-		});
-*/
-    //Setting up the view component
-    $di->set('view', function(){
-        $view = new \Phalcon\Mvc\View();
-        $view->setViewsDir('../app/views/');
-        return $view;
-    });
+	// DB設定
+	$di->set('db', function() {
+		return new \Phalcon\Db\Adapter\Pdo\Sqlite(array(
+			'dbname' => '../app/db/test.sqlite3'
+		));
+	});
 
-    //Handle the request
-    $application = new \Phalcon\Mvc\Application($di);
+	//Setting up the view component
+	$di->set('view', function(){
+		$view = new \Phalcon\Mvc\View();
+		$view->setViewsDir('../app/views/');
+		return $view;
+	});
 
-    echo $application->handle()->getContent();
+	// BaseURI設定
+	// 必須の設定ではありませんが、
+	// ドキュメントルートからPhalconアプリまでのパスが
+	// 環境によって異なる場合は指定しなければなりません。
+	// (例)
+	// 本番(ドキュメントルート直下): http://foo.com/
+	// ローカル(サブフォルダの中): http://localhost/bar/
+	$di->set('url', function(){
+		$url = new Phalcon\Mvc\Url();
+		if (preg_match('/Phalcon/', __DIR__)) {
+			$url->setBaseUri('/Phalcon/');
+		}
+		return $url;
+	});
+
+	//Handle the request
+	$application = new \Phalcon\Mvc\Application($di);
+
+	echo $application->handle()->getContent();
 
 } catch(\Phalcon\Exception $e) {
-     echo "PhalconException: ", $e->getMessage();
+	echo "PhalconException: ", $e->getMessage();
 }
